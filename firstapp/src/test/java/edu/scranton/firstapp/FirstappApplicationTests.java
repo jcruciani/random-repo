3c0bd8cc0d970adf69348b2826ee6bf6fa54a2ee
Joseph Cruciani <joseph.cruciani@scranton.edu>
2020-05-05 10:24:22 -0400

    Initial Commit of an empty project.

---
+++ firstapp/src/test/java/edu/scranton/firstapp/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.scranton.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
